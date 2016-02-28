b22d0142f4336fdfcd6a92df276fd7fbec6afc87
crystal <793832732@qq.com>
2016-02-28 21:00:53 +0800

    modify splitBarcode()

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,46 +1,38 @@
 function printReceipt(inputs) {
-  var itemList = splitBarcode(inputs);
-  var countedBarcode = mergeBarcode(itemList);
-  var cartItems = createCartItems(countedBarcode);
-  var promotionItems = createPromotionItems(cartItems);
-  var receiptItems = createReceiptItems(promotionItems);
-  var shoppingInfo = createShoppingInfo(receiptItems);
-  console.log(shoppingInfo);
+  var itemList = splitBarcode(inputs) ;
+  var countedBarcode = mergeBarcode(itemList) ;
+  var cartItems = createCartItems(countedBarcode) ;
+  var promotionItems = createPromotionItems(cartItems) ;
+  var receiptItems = createReceiptItems(promotionItems) ;
+  var shoppingInfo = createShoppingInfo(receiptItems) ;
+  console.log(shoppingInfo) ;
 }
 
-function splitBarcode(inputs) {
-  var itemList = [];
-  var barcodeInfo;
-  var number;
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].length>10) {
-      barcodeInfo = inputs[i].substring(0,10);
-      number = parseInt(inputs[i].substring(11));
-    }
-    else {
-      barcodeInfo = inputs[i];
-      number = 1;
-    }
-    var item = {barcode:barcodeInfo,count:number};
-    itemList.push(item);
+function splitBarcode(tags) {
+  var itemList = [] ;
+  tags.forEach(function(tag) {
+      var barcodeArr = tag.split('-') ;
+      var barcode = barcodeArr[0] ;
+      var count = barcodeArr[1] ? parseFloat(barcodeArr[1]) : 1 ;
+      itemList.push( { barcode: barcode,count: count} ) ;
+    }) ;
+    return itemList ;
   }
-  return itemList;
-}
 
 function mergeBarcode(itemList) {
-  var countedBarcode = [];
-  var index = 0;
-  countedBarcode[0] = itemList[0];
-  for (var i=1;i<itemList.length;i++) {
-    if(countedBarcode[index].barcode==itemList[i].barcode) {
-      countedBarcode[index].count += itemList[i].count;
+  var countedBarcode = [] ;
+  var index = 0 ;
+  countedBarcode[0] = itemList[0] ;
+  for ( var i = 1 ; i < itemList.length ; i++ ) {
+    if(countedBarcode[index].barcode == itemList[i].barcode) {
+      countedBarcode[index].count += itemList[i].count ;
     }
     else{
-      index ++;
-      countedBarcode[index] = itemList[i];
+      index ++ ;
+      countedBarcode[index] = itemList[i] ;
     }
   }
-  return countedBarcode;
+  return countedBarcode ;
 }
 
 function createCartItems(countedBarcode) {
